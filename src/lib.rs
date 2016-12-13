80fc0422926768a11b46882a60b3917801cf9fc7
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-12-13 21:02:01 +0000

    re-export

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ extern crate rmp_serialize;
 // extern crate cpython;
 
 mod axes2d;
-mod encode;
 mod figure;
 mod scatter;
 
 // re-exports
 pub mod backend;
-pub use axes2d::Axes2D;
+pub mod encode;
+pub use axes2d::{Axes2D, PlotData};
 pub use backend::Backend;
 pub use figure::Figure;
 pub use scatter::Scatter;
