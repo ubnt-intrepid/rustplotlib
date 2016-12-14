334351720ff818ec668b685dbe5c2c0c816711e1
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-12-14 14:53:14 +0000

    add documentation

--- src/backend/mpl_file.rs
+++ src/backend/mpl_file.rs
@@ -15,12 +15,14 @@ const MAIN: &'static str =
   include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/scripts/main.py"));
 
 
+/// Matplotlib backend for saving to the file.
 pub struct MatplotlibFile {
   path: PathBuf,
   fig: Option<Figure>,
 }
 
 impl MatplotlibFile {
+  /// create an instance of MatplotlibFile backend.
   pub fn new<P: AsRef<Path>>(path: P) -> io::Result<MatplotlibFile> {
     Ok(MatplotlibFile {
       path: path.as_ref().to_path_buf(),
@@ -28,6 +30,7 @@ impl MatplotlibFile {
     })
   }
 
+  /// apply all modification to the file.
   pub fn flush(&self) -> io::Result<()> {
     let mut file = OpenOptions::new().write(true)
       .create(true)
@@ -48,6 +51,7 @@ impl MatplotlibFile {
 }
 
 impl Backend for MatplotlibFile {
+  /// replace the internal data of figure.
   fn evaluate(&mut self, fig: &Figure) -> io::Result<&mut Self> {
     self.fig = Some(fig.clone());
     Ok(self)
