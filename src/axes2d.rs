23a456f59f038c1cc35bf10c4cd741e774c46b9e
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-12-15 17:36:21 +0900

    merge

--- src/axes2d.rs
+++ src/axes2d.rs
@@ -1,5 +1,3 @@
-use plotdata::PlotData;
-
 /// Represents an instance of `matplotlib.axes.Axes`.
 #[derive(Debug, Default, RustcEncodable)]
 pub struct Axes2D {
@@ -71,5 +69,62 @@ impl Axes2D {
   pub fn ylim(mut self, lb: f64, ub: f64) -> Self {
     self.config.ylim = Some((lb, ub));
     self
+  }
+}
+
+
+/// Plot type.
+#[derive(Debug, RustcEncodable)]
+pub enum PlotData {
+  Scatter(Scatter),
+}
+
+#[derive(Debug, Default, RustcEncodable)]
+pub struct Scatter {
+  x: Vec<f64>,
+  y: Vec<f64>,
+  config: ScatterConfig,
+}
+
+#[derive(Debug, Default, RustcEncodable)]
+pub struct ScatterConfig {
+  label: Option<String>,
+  color: Option<String>,
+  marker: Option<String>,
+}
+
+impl Scatter {
+  pub fn new(name: &str) -> Scatter {
+    Scatter::default().label(name)
+  }
+
+  pub fn data<X, Y>(mut self, x: X, y: Y) -> Self
+    where X: Into<Vec<f64>>,
+          Y: Into<Vec<f64>>
+  {
+    self.x = x.into();
+    self.y = y.into();
+    self
+  }
+
+  pub fn label(mut self, text: &str) -> Self {
+    self.config.label = Some(text.to_owned());
+    self
+  }
+
+  pub fn color(mut self, color: &str) -> Self {
+    self.config.color = Some(color.to_owned());
+    self
+  }
+
+  pub fn marker(mut self, marker: &str) -> Self {
+    self.config.marker = Some(marker.to_owned());
+    self
+  }
+}
+
+impl From<Scatter> for PlotData {
+  fn from(data: Scatter) -> PlotData {
+    PlotData::Scatter(data)
   }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,11 @@ extern crate rmp_serialize;
 extern crate cpython;
 
 mod axes2d;
-mod plotdata;
 mod figure;
 mod util;
 
 // re-exports
 pub mod backend;
-pub use axes2d::Axes2D;
-pub use plotdata::{PlotData, Scatter};
+pub use axes2d::{Axes2D, PlotData, Scatter};
 pub use backend::Backend;
 pub use figure::Figure;
