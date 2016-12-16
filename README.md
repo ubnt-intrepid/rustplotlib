74a7f417f675328867b216bbdf5eb37ce4134922
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-12-16 18:55:02 +0900

    update README and examples

--- README.md
+++ README.md
@@ -16,6 +16,67 @@ The development version of API documentation is [here](https://ubnt-intrepid.git
 * builder style API
 * support for multiple backends
 
+## Example
+
+```rust
+extern crate rustplotlib;
+
+use rustplotlib::Figure;
+
+fn make_figure<'a>(x: &'a [f64], y1: &'a [f64], y2: &'a [f64]) -> Figure<'a> {
+  use rustplotlib::{Axes2D, Scatter, Line2D, FillBetween};
+
+  let ax1 = Axes2D::new()
+    .add(Scatter::new(r"$y_1 = \sin(x)$")
+      .data(x, y1)
+      .marker("o"))
+    .add(Line2D::new(r"$y_2 = \cos(x)$")
+      .data(x, y2)
+      .color("red")
+      .marker("x")
+      .linestyle("--")
+      .linewidth(1.0))
+    .xlabel("Time [sec]")
+    .ylabel("Distance [mm]")
+    .legend("lower right")
+    .xlim(0.0, 8.0)
+    .ylim(-2.0, 2.0);
+
+  let ax2 = Axes2D::new()
+    .add(FillBetween::new()
+      .data(x, y1, y2)
+      .interpolate(true))
+    .xlim(0.0, 8.0)
+    .ylim(-1.5, 1.5);
+
+  Figure::new()
+    .subplots(2, 1, vec![Some(ax1), Some(ax2)])
+}
+
+fn main() {
+  use std::f64::consts::PI;
+  let x: Vec<f64> = (0..40).into_iter().map(|i| (i as f64) * 0.08 * PI).collect();
+  let y1: Vec<f64> = x.iter().map(|x| x.sin()).collect();
+  let y2: Vec<f64> = x.iter().map(|x| x.cos()).collect();
+
+  let fig = make_figure(&x, &y1, &y2);
+
+  use rustplotlib::Backend;
+  use rustplotlib::backend::Matplotlib;
+  let mut mpl = Matplotlib::new().unwrap();
+  mpl.set_style("ggplot").unwrap();
+  
+  fig.apply(&mut mpl).unwrap();
+  
+  mpl.savefig("simple.png").unwrap();
+  mpl.dump_pickle("simple.fig.pickle").unwrap();
+  mpl.wait().unwrap();
+}
+```
+
+![example](examples/simple.png)
+
+See [examples/simple.rs](examples/simple.rs) for details.
 
 ## License
 This software is released under the MIT license.
--- examples/simple.rs
+++ examples/simple.rs
@@ -6,35 +6,31 @@ use rustplotlib::{backend, Backend};
 use rustplotlib::{Figure, Axes2D, Scatter, Line2D, FillBetween};
 use std::f64::consts::PI;
 
-fn main() {
-  let x: Vec<f64> = (0..40).into_iter().map(|i| (i as f64) * 0.08 * PI).collect();
-  let y1: Vec<f64> = x.iter().map(|x| x.sin()).collect();
-  let y2: Vec<f64> = x.iter().map(|x| x.cos()).collect();
-
+fn make_figure<'a>(x: &'a [f64], y1: &'a [f64], y2: &'a [f64]) -> Figure<'a> {
   let ax1 = Axes2D::new()
-    .add(Scatter::new("sin(x)")
-      .data(&x, &y1)
+    .add(Scatter::new(r"$y_1 = \sin(x)$")
+      .data(x, y1)
       .marker("o"))
-    .add(Line2D::new("cos(x)")
-      .data(&x, &y2)
+    .add(Line2D::new(r"$y_2 = \cos(x)$")
+      .data(x, y2)
       .color("red")
       .marker("x")
       .linestyle("--")
       .linewidth(1.0))
     .xlabel("Time [sec]")
     .ylabel("Distance [mm]")
-    .legend("upper right")
+    .legend("lower right")
     .xlim(0.0, 8.0)
     .ylim(-2.0, 2.0);
-  let ax2 = Axes2D::new().add(FillBetween::new()
-    .data(&x, &y1, &y2)
-    .interpolate(true));
 
-  let fig = Figure::new().subplots(2, 1, vec![Some(ax1), Some(ax2)]);
+  let ax2 = Axes2D::new()
+    .add(FillBetween::new()
+      .data(x, y1, y2)
+      .interpolate(true))
+    .xlim(0.0, 8.0)
+    .ylim(-1.5, 1.5);
 
-  apply_mpl(&fig, "simple.png").unwrap();
-  #[cfg(feature = "native")]
-  apply_mpl_native(&fig, "simple_native.png").unwrap();
+  Figure::new().subplots(2, 1, vec![Some(ax1), Some(ax2)])
 }
 
 fn apply_mpl(fig: &Figure, filename: &str) -> std::io::Result<()> {
@@ -54,4 +50,16 @@ fn apply_mpl_native(fig: &Figure, filename: &str) -> std::io::Result<()> {
   mp.savefig(filename)?;
   mp.dump_pickle(format!("{}.pkl", filename))?;
   Ok(())
+}
+
+fn main() {
+  let x: Vec<f64> = (0..40).into_iter().map(|i| (i as f64) * 0.08 * PI).collect();
+  let y1: Vec<f64> = x.iter().map(|x| x.sin()).collect();
+  let y2: Vec<f64> = x.iter().map(|x| x.cos()).collect();
+
+  let fig = make_figure(&x, &y1, &y2);
+
+  apply_mpl(&fig, "simple.png").unwrap();
+  #[cfg(feature = "native")]
+  apply_mpl_native(&fig, "simple_native.png").unwrap();
 }
